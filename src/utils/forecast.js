ad379b031831e63edc12edaee7833a51db43f2bb
pat-14 <mubarak.a@pavo-tusker.com>
2020-02-10 15:08:39 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,8 +13,8 @@ request({ url, json:true},(error, {body}) => {  // json: true is directly throug
       callback("unable to connect weather service!!", undefined)
     }else if(body.error) {
       callback('Unable to find location', undefined)
-    }else { 
-        callback(undefined,body.daily.data[0].summary+'It is currently'+' '+body.currently.temperature+' '+'is out' + 'There is a'+' '+body.currently.precipProbability+' '+'chance of rain')
+    }else {  
+        callback(undefined,body.daily.data[0].summary+'It is currently'+' '+body.currently.temperature +' '+'is out '+ body.daily.data[0].temperatureHigh + ' '+ ' With a low of '+ ' ' + body.daily.data[0].temperatureLow  +  ' . There is a'+' '+ body.currently.precipProbability +' '+'chance of rain')
         
     }
     })
